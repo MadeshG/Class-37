d29c1e046ebd245b80fc1d84a06a1b33d8935fe4
MadeshG <74277922+MadeshG@users.noreply.github.com>
2021-02-11 21:03:35 -0600

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,34 @@
+class Player{
+    constructor(){
+      this.distance=0
+      this.name=null
+      this.index=null
+
+    }
+    getCount(){
+        var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",function(data){
+      playerCount = data.val();
+    })
+    }
+
+    updateCount(count){
+        database.ref('/').update({
+          playerCount: count
+        });
+      }
+
+      update(){
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+        name:this.name,distance:this.distance
+        });
+      }
+     static getPlayerInfo(){
+        var playerinfo=database.ref('players')
+        playerinfo.on("value",function(data){
+         allPlayers=data.val()
+        })
+      }
+
+}
